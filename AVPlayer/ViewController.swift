efb7aeaedc6cc3b049caf260698a92a8990ba15f
罗嗣鸿 <1658810866@qq.com>
2021-01-27 15:58:26 +0800

    创建工程

---
+++ AVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayer
+//
+//  Created by mac on 2021/1/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
